fb83dffcda2fdc9d009cf77fa06a436b1a0df973
matheusDev <matheus.info29@gmail.com>
2023-10-12 09:42:34 -0300

    Update exercícios Java básico.

---
+++ estrutura-de-classes/src/Estrutura_De_Classes.java
@@ -0,0 +1,18 @@
+public class Estrutura_De_Classes {
+    
+public static void main (String [] args){
+
+    String primeiroNome = "Matheus";
+    String segundoNome = "Santana";
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+    System.out.println("\nOlá seja bem vindo(a)s\n");
+    System.out.println(nomeCompleto);
+    
+}
+
+public static String nomeCompleto ( String primeiroNome, String segundoNome){
+    return primeiroNome.concat(" ").concat(segundoNome+"\n");
+
+}
+
+}
