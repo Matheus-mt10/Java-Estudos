8cc88006dfb2ce9fffb75e050f9c487a438c7aa7
matheusDev <matheus.info29@gmail.com>
2023-10-12 13:52:23 -0300

    Exercício 3 - Laço for

---
+++ dio/src/edu/estudos_dio/exercicios_basicos/exercicio3.java
@@ -0,0 +1,12 @@
+package edu.estudos_dio.exercicios_basicos;
+
+public class exercicio3 {
+    public static void main(String[] args) {
+        // Crie um programa que imprima os números de 1 a 10 usando um loop for.
+       
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
