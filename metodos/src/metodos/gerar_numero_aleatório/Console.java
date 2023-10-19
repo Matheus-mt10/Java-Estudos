59a57867ca7dcedd61623ab0051f3a1e420fbd8c
matheusDev <matheus.info29@gmail.com>
2023-10-19 06:39:18 -0300

    Exercício 2 - Gerar número aleatório

---
+++ metodos/src/metodos/gerar_numero_aleatório/Console.java
@@ -0,0 +1,12 @@
+package metodos.gerar_numero_aleatório;
+
+
+public class Console {
+    public static void main(String[] args) {
+
+        GerarNumeroAleatorio numeroAleatorio = new GerarNumeroAleatorio();
+
+        System.out.println("O número aleatório gerado foi:" + numeroAleatorio.numero);
+        
+    }
+}
---
+++ metodos/src/metodos/gerar_numero_aleatório/GerarNumeroAleatorio.java
@@ -0,0 +1,10 @@
+package metodos.gerar_numero_aleatório;
+
+import java.util.Random;
+
+public class GerarNumeroAleatorio {
+
+    Random random = new Random();
+    int numero= random.nextInt(700);
+
+}
