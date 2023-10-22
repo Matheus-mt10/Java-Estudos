fa59d449cc5ce870baf92aab1b4bda06e758ecc9
matheusDev <matheus.info29@gmail.com>
2023-10-22 02:16:31 -0300

    Exercícios

---
+++ exercicios/ifElse/positivo_negativo/PositivoNegativo.java
@@ -0,0 +1,21 @@
+package exercicios.ifElse.positivo_negativo;
+import java.util.Scanner;
+
+public class PositivoNegativo {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite um número para verificação: ");
+        int numero = sc.nextInt();
+
+        if (numero<0) {
+            System.out.println("O número digitado foi "+numero +" portanto é negativo.");
+        } else {
+            System.out.println("O número digitado foi "+numero +" portanto é Positivo.");
+        }
+
+
+        
+    }
+}
